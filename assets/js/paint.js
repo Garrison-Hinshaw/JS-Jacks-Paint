7b38822386366285722ecfb12dd078c6ea1918a8
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-07-12 17:17:58 -0400

    added events to event listeners

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -3,8 +3,8 @@ function configureListeners() {
 
 
      for (var i = 0; i < images.length; i++) {        
-        document.getElementById(images[i].id).addEventListener('mouseover')// iterate over images and add mouseover event listeners
-        document.getElementById(images[i].id).addEventListener('mouseover')      
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)// iterate over images and add mouseover event listeners
+        document.getElementById(images[i].id).addEventListener('mouseover', removeOpacity, false)      
     } 
 }
 
